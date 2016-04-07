551afd9b5829c9483fb128a52f60908207f2dbe9
ZhouFuxin <497595927@qq.com>
2016-04-07 23:53:10 +0800

    task17

--- zfxdev2_13(-17)/js/2_17.js
+++ zfxdev2_13(-17)/js/2_17.js
@@ -56,6 +56,35 @@ var pageState = {
  */
 function renderChart() {
 
+  var chartObj = document.getElementsByClassName("aqi-chart-wrap")[0];
+
+  if (chartObj == null) {
+
+    console.log("not load page!!!");
+    return;
+
+  } else {
+
+    var ulObj = chartObj.getElementsByTagName("ul")[0];
+
+    // 重置图表
+    while(ulObj.lastChild) {
+      ulObj.removeChild(ulObj.lastChild);
+    }
+
+    for (date in chartData.data) {
+      var itemObj       = document.createElement("li");
+          itemProgress  = document.createElement("div");
+
+      ulObj.appendChild(itemObj);
+
+      itemObj.appendChild(itemProgress);
+      itemObj.style.width = 100/chartData.count + "%";
+      itemObj.setAttribute("title", date + "的空气质量: " + chartData.data[date]);
+
+      itemProgress.style.height = chartData.data[date]/5 + "%";
+    }
+  }
 }
 
 /**
@@ -63,21 +92,143 @@ function renderChart() {
  */
 function graTimeChange() {
   // 确定是否选项发生了变化 
+  console.log(this.value);
 
+  /*
+  var isNotChange = pageState.nowGraTime == this.value;
+  if (isNotChange) {
+    return;
+  } else {
+    pageState.nowGraTime = this.value;
+  }
+  */
+  
   // 设置对应数据
+  var allData = {};
+  chartData.data = {};
+
+  var citySelectObj = document.getElementById("city-select");
+
+  pageState.nowGraTime = this.value;
+  allData = aqiSourceData[citySelectObj.value];
+  /*
+  var selectIndex = 0;
+  for (city in aqiSourceData) {
+
+    if (pageState.nowSelectCity == selectIndex) {
+
+      allData = aqiSourceData[city];
+    }
+
+    selectIndex++;
+  }
+  */
+
+  var dat = new Date("2016-01-01");
+
+
+  if (pageState.nowGraTime == "day") {
+
+    chartData.data = allData;
+
+  } else if (pageState.nowGraTime == "week") {
+
+    var daysIndex   = 1;
+    var weeksIndex  = 1;
+    var daysInWeek  = 1;
+    var isFullWeek  = false;
+    var sumOfWeek   = 0; 
+
+    for(date in allData) {
+
+      sumOfWeek += allData[date];
+
+      var week = new Date(date).getDay();
+
+      if ( week == 0 || daysIndex == 91) {
+
+        isFullWeek = true;
+
+      } else {
+
+        isFullWeek = false;
+
+      }
+
+      if (isFullWeek) {
+        chartData.data["第" + weeksIndex + "周"] = Math.floor(sumOfWeek / daysInWeek);
+        
+        sumOfWeek = 0;
+        daysInWeek = 0;
+        weeksIndex++;
+      }
+
+      daysIndex++;
+      daysInWeek++;
+    }
+
+
+  } else if (pageState.nowGraTime == "month") {
+
+    var daysIndex   = 1;
+    var monthsIndex  = 1;
+    var daysInMonth  = 1;
+    var isFullMonth  = false;
+    var sumOfMonth   = 0; 
+
+    for(date in allData) {
+
+      sumOfMonth += allData[date];
+
+      var today = new Date(date);
+      var nextDate = new Date(date);
+      nextDate.setDate(nextDate.getDate() + 1);
+
+      if ( nextDate.getDate() == 1 || daysIndex == 91) {
+
+        isFullMonth = true;
+
+      } else {
+
+        isFullMonth = false;
+
+      }
+
+      if (isFullMonth) {
+        chartData.data["第" + monthsIndex + "月"] = Math.floor(sumOfMonth / daysInMonth);
+        
+        sumOfMonth = 0;
+        daysInMonth = 0;
+        monthsIndex++;
+      }
+
+
+      daysIndex++;
+      daysInMonth++;
+    }
+
+  } else {
+    console.log("get radio value error");
+  }
+
+  chartData.count = getObjectLength(chartData.data);
 
   // 调用图表渲染函数
+  renderChart();
 }
 
 /**
  * select发生变化时的处理函数
  */
 function citySelectChange() {
-  // 确定是否选项发生了变化 
+  // 确定是否选项发生了变化
+  console.log(this.value);
 
   // 设置对应数据
+  chartData.data = aqiSourceData[this.value];
 
   // 调用图表渲染函数
+  renderChart();
 }
 
 /**
@@ -85,15 +236,37 @@ function citySelectChange() {
  */
 function initGraTimeForm() {
 
+  var graTimeObj = document.getElementById("form-gra-time");
+  var radioObjs = graTimeObj.getElementsByTagName("input");
+
+  for (var i = 0; i < radioObjs.length; i++) {
+    eventUtil.addHandler(radioObjs[i], "change", graTimeChange);
+  }
+
 }
 
 /**
  * 初始化城市Select下拉选择框中的选项
  */
 function initCitySelector() {
+
+  var citySelectObj = document.getElementById("city-select");
+
   // 读取aqiSourceData中的城市，然后设置id为city-select的下拉列表中的选项
+  while(citySelectObj.lastChild) {
+    citySelectObj.removeChild(citySelectObj.lastChild);
+  }
+
+  for (city in aqiSourceData) {
+    var optionObj = document.createElement("option");
+    var optionContent = document.createTextNode(city);
+
+    optionObj.appendChild(optionContent);
+    citySelectObj.appendChild(optionObj);
+  }
 
   // 给select设置事件，当选项发生变化时调用函数citySelectChange
+  eventUtil.addHandler(citySelectObj, "change", citySelectChange);
 
 }
 
@@ -103,7 +276,32 @@ function initCitySelector() {
 function initAqiChartData() {
   // 将原始的源数据处理成图表需要的数据格式
   // 处理好的数据存到 chartData 中
+  var citySelectObj = document.getElementById("city-select");
+
+  var city = citySelectObj.value;
+
+  chartData.count = getObjectLength(aqiSourceData[city]);
+  chartData.data = aqiSourceData[city];
+
+  renderChart();
 }
+
+
+/**
+ * 获取对象长度
+ */
+function getObjectLength(obj) {
+
+  var count = 0;
+
+  for (iter in obj) {
+
+    count ++;
+  }
+
+  return count;
+}
+
 
 /**
  * 初始化函数
@@ -114,4 +312,4 @@ function init() {
   initAqiChartData();
 }
 
-init();
+window.onload = init;
